92014dc4b0579fdc712ccce1d0157a6213c317df
Adam <2165086@slu.edu.ph>
2019-04-08 12:49:49 +0700

    Added examples in Css

--- Learn-HTML-CSS-Javascript/Other-Html/JavaScript/functions.js
+++ Learn-HTML-CSS-Javascript/Other-Html/JavaScript/functions.js
@@ -9,4 +9,3 @@ function ifrmOut() {
 function back() {
     goBack.href=document.referrer;
 }
-//="<svg width='400' height='180'><rect x='50' y='20' rx='20 ry='20' width='150' height='150' style='fill:red;stroke:black;stroke-width:5;opacity:0.5'/><svg>"
--- Learn-HTML-CSS-Javascript/Other-Html/JavaScript/functionsCss.js
+++ Learn-HTML-CSS-Javascript/Other-Html/JavaScript/functionsCss.js
@@ -1,7 +1,14 @@
+// refactor this 
 var in1 = document.getElementById("text1");
 var in2 = document.getElementById("text2");
+var in3 = document.getElementById("text3");
+var in4 = document.getElementById("text4");
 var frCss = document.getElementById("framecss");
+var frCss1 = document.getElementById("framecss1")
 
 function run() {
     frCss.srcdoc =  in1.value+in2.value;
+}
+function run1() {
+    frCss1.srcdoc = in3.value+in4.value;
 }
